02fc31f040b501c340f98e291555ce89a4f70dcd
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-22 10:33:25 -0300

    Adicionar arquivos de projeto.

---
+++ 02 - MANUAL - For/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace _02___MANUAL___For {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Mostre os ímpares de 1 até X, um valor por linha.");
+            int x = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= x; i++) {
+                if(i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
