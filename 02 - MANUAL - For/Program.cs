c69c150db547e06b3d507106a60e51364d30be86
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-22 10:45:48 -0300

    Commit - E.2

--- 02 - MANUAL - For/Program.cs
+++ 02 - MANUAL - For/Program.cs
@@ -2,14 +2,24 @@
 namespace _02___MANUAL___For {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Mostre os ímpares de 1 até X, um valor por linha.");
-            int x = int.Parse(Console.ReadLine());
-            for(int i = 1; i <= x; i++) {
-                if(i % 2 != 0) {
-                    Console.WriteLine(i);
+            Console.WriteLine("Mostre quantos destes valores X estão dentro do intervalo [10,20].");
+            Console.WriteLine("------------------------------");
+            Console.Write("Digite o tamanho do intervalo: "); int n = int.Parse(Console.ReadLine());
+            int contIn = 0;
+            int contOut = 0;
+            
+            for(int i = 0; i < n; i++) {
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <= 20) {
+                    contIn = contIn + 1;
+                }
+                else {
+                    contOut = contOut + 1;
                 }
             }
+            Console.WriteLine("------------------------------");
+            Console.WriteLine(contIn +" in");
+            Console.WriteLine(contOut + " out");
         }
-
     }
 }
