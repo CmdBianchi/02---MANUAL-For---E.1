c45b66d3166829885183355d0bfbc06ddb386c5d
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-23 09:11:33 -0300

    Commit - E.06

--- 02 - MANUAL - For/Program.cs
+++ 02 - MANUAL - For/Program.cs
@@ -4,11 +4,12 @@ namespace _02___MANUAL___For {
         static void Main(string[] args) {
             Console.WriteLine("Exercício 05 - Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.");
             Console.Write("Digite o tamanho do FATORIAL: "); int n = int.Parse(Console.ReadLine()); //----> Números de casos
-            int fat = 1;
+            
             for (int i = 1; i <= n; i++) {
-                fat = fat * i;                
-            }
-            Console.WriteLine(fat);
+                if(n % i == 0) {
+                    Console.WriteLine(i);
+                }
+            }           
         }
     }
 }
