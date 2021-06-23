bc519948fd9f9b2e37599702370415e366811efb
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-23 09:02:11 -0300

    Commit - E.05

--- 02 - MANUAL - For/Program.cs
+++ 02 - MANUAL - For/Program.cs
@@ -2,22 +2,13 @@
 namespace _02___MANUAL___For {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Exercício 04 - estrutura repetitiva FOR - Lista de exercícios, mostre a divisão do primeiro pelo segundo.Se o denominador for igual a zero, mostrar a mensagem divisao impossivel.");
-            int n = int.Parse(Console.ReadLine()); //----> Números de casos
-
+            Console.WriteLine("Exercício 05 - Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.");
+            Console.Write("Digite o tamanho do FATORIAL: "); int n = int.Parse(Console.ReadLine()); //----> Números de casos
+            int fat = 1;
             for (int i = 1; i <= n; i++) {
-                Console.Write("Digite o valor X: "); double x = double.Parse(Console.ReadLine());
-                Console.Write("Digite o valor Y: "); double y = double.Parse(Console.ReadLine());
-
-                if(y == 0) {
-                    Console.WriteLine("Impossivel dividir");
-                }
-                else {
-                    double z = x / y;
-                    Console.WriteLine("------------------");
-                    Console.WriteLine("Resultado: "+z);
-                }
+                fat = fat * i;                
             }
+            Console.WriteLine(fat);
         }
     }
 }
