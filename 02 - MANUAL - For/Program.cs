01188ffa517ed24b397a077cdd66e2569a3b4e9b
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-23 09:43:19 -0300

    Commit - E.08

--- 02 - MANUAL - For/Program.cs
+++ 02 - MANUAL - For/Program.cs
@@ -2,16 +2,16 @@
 namespace _02___MANUAL___For {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Exercício 05 - Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.");
-            Console.Write("Digite o tamanho do FATORIAL: "); int n = int.Parse(Console.ReadLine()); //----> Números de casos
-            
+            Console.WriteLine("Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme exemplo.");
+            Console.Write("Digite um número inteiro positivo N: "); int n = int.Parse(Console.ReadLine()); //----> Números de casos
+
+            int soma = 0;
             for (int i = 1; i <= n; i++) {
-                int frist = i;
-                int second = i * i;
-                int third = i * i * i;
-                Console.WriteLine("-------------");
-                Console.WriteLine($"{frist} {second} {third}");
-            }           
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine("Soma = " + soma);
         }
     }
 }
