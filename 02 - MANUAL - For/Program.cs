6dfe097da043cdd721cabef5753943d3df40fde6
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-23 08:52:00 -0300

    Commit - E.04

--- 02 - MANUAL - For/Program.cs
+++ 02 - MANUAL - For/Program.cs
@@ -2,24 +2,22 @@
 namespace _02___MANUAL___For {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Mostre quantos destes valores X estão dentro do intervalo [10,20].");
-            Console.WriteLine("------------------------------");
-            Console.Write("Digite o tamanho do intervalo: "); int n = int.Parse(Console.ReadLine());
-            int contIn = 0;
-            int contOut = 0;
-            
-            for(int i = 0; i < n; i++) {
-                int x = int.Parse(Console.ReadLine());
-                if (x >= 10 && x <= 20) {
-                    contIn = contIn + 1;
+            Console.WriteLine("Exercício 04 - estrutura repetitiva FOR - Lista de exercícios, mostre a divisão do primeiro pelo segundo.Se o denominador for igual a zero, mostrar a mensagem divisao impossivel.");
+            int n = int.Parse(Console.ReadLine()); //----> Números de casos
+
+            for (int i = 1; i <= n; i++) {
+                Console.Write("Digite o valor X: "); double x = double.Parse(Console.ReadLine());
+                Console.Write("Digite o valor Y: "); double y = double.Parse(Console.ReadLine());
+
+                if(y == 0) {
+                    Console.WriteLine("Impossivel dividir");
                 }
                 else {
-                    contOut = contOut + 1;
+                    double z = x / y;
+                    Console.WriteLine("------------------");
+                    Console.WriteLine("Resultado: "+z);
                 }
             }
-            Console.WriteLine("------------------------------");
-            Console.WriteLine(contIn +" in");
-            Console.WriteLine(contOut + " out");
         }
     }
 }
